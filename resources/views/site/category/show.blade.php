c2113a6a90d30556ec3b4b6bbcc47f8490ff4eca
Renato <rntdek@gmail.com>
2021-09-03 03:16:26 -0300

    static pages/assets

--- resources/views/site/category/show.blade.php
+++ resources/views/site/category/show.blade.php
@@ -1,1 +1,54 @@
-categoria {{$slug}}
+@extends('layouts.site')
+
+@section('content')
+    <header class="common-header">
+        <div class="header-wrapper">
+            <h1 class="title-large">With apples drink teriyaki. </h1>
+        </div>
+        <div class="pattern__header"></div>
+    </header>
+
+    <div class="products__general main-wrapper">
+
+        <aside class="product__selected">
+            <article class="card__product">
+                <a href="products-detail.php">
+                    <div class="card__cover">
+
+                    </div>
+                    <header class="card__product-header">
+
+                    </header>
+                </a>
+            </article>
+            <a class="back-to-products" href="{{route('site.products')}}">Voltar</a>
+        </aside>
+
+        <section class="products__list">
+            <header>
+                <h2 class="title-large">Cum quadra ridetis, omnes visuses imitari regius, brevis brodiumes. </h2>
+                <p>Ususs favere! Tabes grandis palus est. Sunt mensaes magicae fatalis, placidus genetrixes. Cum musa unda, omnes amores attrahendam pius, noster particulaes. </p>
+            </header>
+            <article class="product">
+                <a class="clickable-area" href="javascript:;">
+                    <header class="product__header">
+                        <h3 class="title-medium">Item</h3>
+                        <!-- Tooggle item -->
+                        <span class="collapse__open"></span>
+                    </header>
+                        <img class="exclusive__label"  alt="Etiqueta de
+                        produto
+                        exclusivo">
+                </a>
+
+                <section class="product__content">
+
+                    <div class="product__desciption">
+                        Description
+                    </div>
+
+                </section>
+            </article>
+        </section>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Site\BlogController;
 use App\Http\Controllers\Site\CategoryController;
 use App\Http\Controllers\Site\ContactController;
 use App\Http\Controllers\Site\HomeController;
+use App\Http\Controllers\Site\ServicesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,15 +19,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::namespace('Site')->group(function() {
-   Route::get('/', [HomeController::class, '__invoke']);
+   Route::get('/', [HomeController::class, '__invoke'])->name('site.home');
 
-   Route::get('/produtos', [CategoryController::class, 'index']);
-   Route::get('/produtos/{slug}', [CategoryController::class, 'show']);
+   Route::get('/produtos', [CategoryController::class, 'index'])->name('site.products');
+   Route::get('/produtos/{slug}', [CategoryController::class, 'show'])->name('site.products.category');
 
-   Route::get('/blog', [BlogController::class, '__invoke']);
+   Route::get('/blog', [BlogController::class, '__invoke'])->name('site.blog');
 
-   Route::view('/sobre', 'site.about.index');
+   Route::view('/sobre', 'site.about.index')->name('site.about');
 
-   Route::get('/contato', [ContactController::class, 'index']);
-   Route::post('/contato', [ContactController::class, 'form']);
+   Route::get('/contato', [ContactController::class, 'index'])->name('site.contact');
+   Route::post('/contato', [ContactController::class, 'form'])->name('site.contact.form');
 });
